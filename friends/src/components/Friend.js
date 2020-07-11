92c60aa7a71ae668db5e53f3aa3bc29cc2bb750a
Paul H Menard Jr <clmonitor2013@gmail.com>
2020-07-11 01:17:26 -0400

    built friend component

---
+++ friends/src/components/Friend.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../css/index.css";
+
+const Friend = () => {
+    return (
+        <div className="friend">
+            <div className="container">
+                <h1>Name: Paul Menard</h1>
+                <p>Age: 46</p>
+                <p>Email: me@yahoo.com</p>
+            </div>
+        </div>
+    );
+
+}
+
+export default Friend;
