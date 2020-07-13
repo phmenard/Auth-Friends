af9859705e3c412284df82a64c3960d09e4e7322
Paul H Menard Jr <clmonitor2013@gmail.com>
2020-07-13 18:22:43 -0400

    added new friend

---
+++ friends/src/components/NewFriend.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import "../css/index.css";
+import { axiosWithAuth } from "./utils/asiosWithAuth";
+
+const NewFriend = () => {
+
+    const [newFriend, setNewFriend] = useState({
+        id: "",
+        name: "",
+        age: "",
+        emial: ""
+    });
+
+    const addFriend = (event) => {
+        event.preventDefault();
+        console.log(newFriend);
+        axiosWithAuth().post('friends', newFriend)
+            .then(res => {
+                console.log(res);
+                
+            })
+    }
+
+    const handleChange = e => {
+        setNewFriend({
+            ...newFriend,
+            [e.target.name]: e.target.value
+        });
+
+
+    };
+
+    return (
+        <div className="friends-list">
+            <div className="friend">
+                <div className="container">
+                    <div><form onSubmit={addFriend}>
+                        <label htmlFor="name">
+                            <input
+                                type="text"
+                                name="name"
+                                placeholder="name"
+                                value={newFriend.username}
+                                onChange={handleChange}
+                            /></label>
+                        <label htmlFor="age">
+                            <input
+                                type="text"
+                                name="age"
+                                placeholder="age"
+                                value={newFriend.age}
+                                onChange={handleChange}
+                            /></label>
+                        <label htmlFor="email">
+                            <input
+                                type="text"
+                                name="email"
+                                placeholder="emial"
+                                value={newFriend.email}
+                                onChange={handleChange}
+                            /></label>
+                        <button>Add Friend</button>
+                    </form></div>
+                </div>
+            </div>
+        </div>
+    );
+
+}
+
+export default NewFriend;
