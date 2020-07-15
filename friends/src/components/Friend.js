b0de9798609138c6f0550eddd753b9695f6ecb9b
Paul H Menard Jr <clmonitor2013@gmail.com>
2020-07-14 21:08:30 -0400

    Added remove friend

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -1,7 +1,27 @@
 import React from "react";
 import "../css/index.css";
+import { IoIosCloseCircle, IoMdCreate } from "react-icons/io";
+import { axiosWithAuth } from "./utils/asiosWithAuth";
+import { useHistory } from "react-router-dom"
 
 const Friend = (props) => {
+    let history = useHistory();
+
+    const removeFriend = () => {
+        if (window.confirm('Are you sure you want to remove this friend?')) {
+            axiosWithAuth().delete(`friends/${props.friend.id}`)
+            .then(res => {
+                console.log(res);
+                //history.push("/");
+                props.getFriends();
+            })
+    
+            
+        } else {
+            // Do nothing!
+        }
+    }
+
     return (
         <div className="friend">
             <div className="container">
@@ -9,6 +29,8 @@ const Friend = (props) => {
                 <p>Age: {props.friend.age}</p>
                 <p>Email: {props.friend.email}</p>
             </div>
+            <div className="friend-icons"><IoMdCreate className="edit"/><IoIosCloseCircle className="remove" onClick={removeFriend}/></div>
+            
             
             
             
